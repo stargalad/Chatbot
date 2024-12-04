d89641304378d1ef9c9464f01489e64296e00350
Sidharth Kumar <85510647+stargalad@users.noreply.github.com>
2024-12-04 19:34:26 +1030

    Update code-1.py

--- code-1.py
+++ code-1.py
@@ -1,6 +1,6 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 model_name = "facebook/blenderbot-400M-distill"
-# Load model (download on first run and reference local installation for consequent runs)
+# Load model 
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 conversation_history = []
@@ -20,13 +20,13 @@ while True:
     # Create conversation history string
     history_string = "\n".join(conversation_history)
 
-    # Get the input data from the user
+    # Get input data from user
     input_text = input("> ")
 
-    # Tokenize the input text and history
+    # Tokenize input text and history
     inputs = tokenizer.encode_plus(history_string, input_text, return_tensors="pt")
 
-    # Generate the response from the model
+    # Generate response from the model
     outputs = model.generate(**inputs)
 
     # Decode the response
